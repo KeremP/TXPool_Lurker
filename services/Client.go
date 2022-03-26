efb64fdb8f232c5f898bffb8b5f6c99e4aeb24a3
KeremP <kerpro26@gmail.com>
2022-03-26 18:59:18 -0400

    dependency update. WIP changes to client/txstreaming

--- services/Client.go
+++ services/Client.go
@@ -1,44 +1,67 @@
 package services
 
 import (
+	"fmt"
+	"os"
+	"reflect"
+	"unsafe"
 
-        // "flag"
-        // "fmt"
-        "log"
-        "os"
-        "reflect"
-        "unsafe"
-        "github.com/ethereum/go-ethereum/ethclient"
-        "github.com/ethereum/go-ethereum/rpc"
-        "github.com/joho/godotenv"
-
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/joho/godotenv"
 )
 
+type (
+	bee struct {
+		Address string `json:"addr"`
+		PK      string `json:"pk"`
+	}
+
+	logHandler struct {
+		format log.Format
+	}
+)
+
+func (l *logHandler) Log(r *log.Record) error {
+	fmt.Print(string(l.format.Format(r)))
+	return nil
+}
+
+func configureLog(level log.Lvl) {
+	glog := log.NewGlogHandler(&logHandler{
+		format: log.TerminalFormat(true),
+	})
+
+	glog.Verbosity(level)
+	log.Root().SetHandler(glog)
+}
+
 func getEnvVariable(key string) string {
-  err:= godotenv.Load("../.env")
-  if err != nil {
-    log.Fatalf("Error loading .env file")
-  }
-  return os.Getenv(key)
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	return os.Getenv(key)
 }
 
 // Init client to handle communication with GETH protocol
 
 var endpointURL = getEnvVariable("RPC_ENDPOINT")
 
-func DialClient() *ethclient.Client {
-  client, err := ethclient.Dial(endpointURL)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  return client
+func newRPCClient() *ethclient.Client {
+	client, err := ethclient.Dial(endpointURL)
+	if err != nil {
+		panic(err)
+	}
+	return client
 }
 
 func InitRPCClient() *rpc.Client {
-  var clientVal reflect.Value
-  clientVal = reflect.ValueOf(DialClient()).Elem()
-  fieldStruct := clientVal.FieldByName("c")
-  clientPointer := reflect.NewAt(fieldStruct.Type(), unsafe.Pointer(fieldStruct.UnsafeAddr())).Elem()
-  finalClient, _ := clientPointer.Interface().(*rpc.Client)
-  return finalClient
+	var clientVal reflect.Value
+	clientVal = reflect.ValueOf(newRPCClient()).Elem()
+	fieldStruct := clientVal.FieldByName("c")
+	clientPointer := reflect.NewAt(fieldStruct.Type(), unsafe.Pointer(fieldStruct.UnsafeAddr())).Elem()
+	finalClient, _ := clientPointer.Interface().(*rpc.Client)
+	return finalClient
 }
--- services/TxStream.go
+++ services/TxStream.go
@@ -1,45 +1,45 @@
 package services
 
 import (
-        "context"
-        "fmt"
+	"context"
+	"fmt"
 
-        "github.com/ethereum/go-ethereum/common"
-        "github.com/ethereum/go-ethereum/core/types"
-        // "github.com/ethereum/go-ethereum/ethclient"
-        "github.com/ethereum/go-ethereum/rpc"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 
+	// "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 )
 
 func StreamTx(rpcClient *rpc.Client) {
 
-  TxChannel := make(chan common.Hash)
+	TxChannel := make(chan common.Hash)
 
-  rpcClient.EthSubscribe(
-    context.Background(), TxChannel, "newPendingTransactions",
-  )
+	rpcClient.EthSubscribe(
+		context.Background(), TxChannel, "newPendingTransactions",
+	)
 
-  client := DialClient()
-  fmt.Println("Subscribed to mempool")
+	client := newRPCClient()
+	fmt.Println("Subscribed to mempool")
 
-  chainID, _ := client.NetworkID(context.Background())
+	chainID, _ := client.NetworkID(context.Background())
 
-  signer := types.NewEIP155Signer(chainID)
+	signer := types.NewEIP155Signer(chainID)
 
-  for {
-    select{
+	for {
+		select {
 
-    case transactionHash := <-TxChannel:
-      tx, pending, _ := client.TransactionByHash(context.Background(), transactionHash)
-      if pending {
-        _,_ = signer.Sender(tx)
-        handleTX(tx)
-      }
-    }
-  }
+		case transactionHash := <-TxChannel:
+			tx, pending, _ := client.TransactionByHash(context.Background(), transactionHash)
+			if pending {
+				_, _ = signer.Sender(tx)
+				handleTX(tx)
+			}
+		}
+	}
 }
 
 func handleTX(tx *types.Transaction) {
-    // fmt.Println("New TX detected. Hash: ", tx.Hash().Hex())
-    classifyTx(tx)
+	// fmt.Println("New TX detected. Hash: ", tx.Hash().Hex())
+	classifyTx(tx)
 }
